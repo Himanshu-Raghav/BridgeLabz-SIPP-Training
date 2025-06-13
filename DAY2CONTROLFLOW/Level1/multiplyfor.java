96724aebdb9b6458a4d93a8a3c16fc2a2653af93
Himanshu-Raghav <himanshud310@gmail.com>
2025-06-13 21:11:43 +0530

    [Himanshu] Added a program to find all the multiple of a number taken as user input below 100 using for loop

---
+++ DAY2CONTROLFLOW/Level1/multiplyfor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class multiplyfor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        if (number > 0 && number < 100) {
+            for (int i = 100; i >= 1; i--) {
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Invalid input. Number must be > 0 and < 100");
+        }
+    }
+}
