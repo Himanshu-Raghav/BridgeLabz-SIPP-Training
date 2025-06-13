aa20cd8ec9919cd8140f87160c3f26bba2672155
Himanshu-Raghav <himanshud310@gmail.com>
2025-06-13 21:28:24 +0530

    [Himanshu] Added a Program to count digits in the given number

---
+++ DAY2CONTROLFLOW/Level1/countdigits.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class countdigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int count = 0;
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+        System.out.println("Number of digits: " + count);
+    }
+}
