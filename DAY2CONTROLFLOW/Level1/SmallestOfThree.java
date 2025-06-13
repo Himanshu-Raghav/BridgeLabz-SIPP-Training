7cc2001db94a922b5e794b1a98758cca2ccc7156
Himanshu-Raghav <himanshud310@gmail.com>
2025-06-13 19:48:08 +0530

    [Himanshu] Added Is first the smallest number?
    '

---
+++ DAY2CONTROLFLOW/Level1/SmallestOfThree.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SmallestOfThree {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+        boolean isSmallest = num1 < num2 && num1 < num3;
+        System.out.println("Is the first number the smallest? " + isSmallest);
+    }
+}
