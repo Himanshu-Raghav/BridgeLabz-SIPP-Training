1e5e63ee0942e0c6896ca01e2f22af49d83a2055
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-16 10:55:58 +0530

    Update Multiplicationtable.java
    [Himanshu] Added a program to print a multiplication table of a number.

--- DAY3ARRAYS/Multiplicationtable.java
+++ DAY3ARRAYS/Multiplicationtable.java
@@ -9,7 +9,7 @@ public class Multiplicationtable {
         int[] table = new int[10];
 
         for (int i = 1; i <= 10; i++) {
-            table[i - 1] = number * i;
+            table[i - 1] =number * i;
         }
 
         for (int i = 1; i <= 10; i++) {
