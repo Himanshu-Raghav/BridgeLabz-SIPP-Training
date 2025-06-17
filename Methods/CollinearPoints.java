08841377bf0d775992de126c14ed33825f6d9c02
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-17 11:10:01 +0530

    [Himanshu] Added a program to find the 3 points that are collinear using the slope formulae and area of triangle formulae.

--- Methods/CollinearPoints.java
+++ Methods/CollinearPoints.java
@@ -11,6 +11,7 @@ public class CollinearPoints {
     public static boolean areCollinearArea(double x1, double y1, double x2, double y2, double x3, double y3) {
         double area = 0.5 * Math.abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
         return Math.abs(area) < 1e-10;
+        
     }
 
     public static void main(String[] args) {
