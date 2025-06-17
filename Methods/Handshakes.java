0c21e6dfb4d06a7f314a6d8eca621cea7dc50f67
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-17 11:28:52 +0530

    [Himanshu] Added a program to find the maximum number of handshakes among the students

--- Methods/Handshakes.java
+++ Methods/Handshakes.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Handshakes {
     public static int calculateHandshakes(int numberOfStudents) {
         return (numberOfStudents * (numberOfStudents - 1)) / 2;
+        
     }
 
     public static void main(String[] args) {
