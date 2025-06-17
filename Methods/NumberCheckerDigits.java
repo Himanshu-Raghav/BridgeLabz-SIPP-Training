8db4e7acce15eb5fe72f7ac4ba6d408e578d8214
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-17 11:39:32 +0530

    [Himanshu] Added o find the count of digits in the number and a Method to store the digits of the number in a digits array

--- Methods/NumberCheckerDigits.java
+++ Methods/NumberCheckerDigits.java
@@ -10,6 +10,7 @@ public class NumberCheckerDigits {
         int[] digits = new int[numStr.length()];
         for (int i = 0; i < numStr.length(); i++) {
             digits[i] = numStr.charAt(i) - '0';
+            
         }
         return digits;
     }
