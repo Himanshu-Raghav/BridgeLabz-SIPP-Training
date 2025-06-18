aad6a2776b2319c18dcff058a8108dfe042ec9f0
Himanshu-Raghav <himanshud310@gmail.com>
2025-06-18 19:09:28 +0530

    first

--- Strings /Strings/ArrayIndexOutOfBoundsDemo.java
+++ Strings /Strings/ArrayIndexOutOfBoundsDemo.java
@@ -1,11 +1,9 @@
-public class ArrayIndexOutOfBound {
-    // Method to generate ArrayIndexOutOfBoundsException
+public class ArrayIndexOutOfBoundsDemo {
+    
     public static void generateException(String[] names) {
-        // Accessing index out of bounds
+       
         System.out.println(names[names.length]);
     }
-
-    // Method to handle ArrayIndexOutOfBoundsException
     public static void handleException(String[] names) {
         try {
             System.out.println(names[names.length]);
@@ -18,8 +16,6 @@ public class ArrayIndexOutOfBound {
 
     public static void main(String[] args) {
         String[] names = {"Alice", "Bob", "Charlie"};
-        // Uncomment to see the exception
-        // generateException(names);
         System.out.println("Demonstrating ArrayIndexOutOfBoundsException handling:");
         handleException(names);
     }
--- Strings /Strings/CompareStringsCharAt.java
+++ Strings /Strings/CompareStringsCharAt.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
-public class CompareStrings {
-    // Method to compare two strings using charAt
+public class CompareStringsCharAt {
     public static boolean compareByCharAt(String s1, String s2) {
         if (s1.length() != s2.length()) return false;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) return false;
-            
         }
         return true;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first string: ");
@@ -20,7 +17,6 @@ public class CompareStrings {
 
         boolean userResult = compareByCharAt(str1, str2);
         boolean builtInResult = str1.equals(str2);
-
         System.out.println("User-defined comparison: " + userResult);
         System.out.println("Built-in equals() comparison: " + builtInResult);
         System.out.println("Are both results same? " + (userResult == builtInResult));
