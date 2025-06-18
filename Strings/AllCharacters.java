315ba6f1b04c00e9e3fdbd250f314058e53e2ea8
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-18 19:17:31 +0530

    [Himanshu] Added  a program to return all the characters in a string using the user-defined method

--- Strings/AllCharacters.java
+++ Strings/AllCharacters.java
@@ -5,6 +5,7 @@ public class AllCharacters {
         char[] chars = new char[str.length()];
         for (int i = 0; i < str.length(); i++) {
             chars[i] = str.charAt(i);
+            
         }
         return chars;
     }
