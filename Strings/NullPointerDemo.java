69c872eb71966f10d4fadf591887ceced62283a7
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-18 19:24:26 +0530

    [Himanshu] Added  a program for nullpointerexception

--- Strings/NullPointerDemo.java
+++ Strings/NullPointerDemo.java
@@ -1,31 +1,20 @@
 public class NullPointerDemo {
-    
-    public static void generateNullPointer() {
+
+    public static void generateException() {
         String text = null;
-        
         System.out.println(text.length());
-<<<<<<< HEAD:Strings /Strings/NullPointerDemo.java
-    }   
-=======
     }
 
-   
->>>>>>> 3432d985465bb1a35e973a31f226f071c2136dc4:Strings/NullPointer.java
-    public static void handleNullPointer() {
+    public static void demonstrateNullPointerException() {
         String text = null;
         try {
             System.out.println(text.length());
         } catch (NullPointerException e) {
-            System.out.println("Caught NullPointerException: " + e.getMessage());
+            System.out.println("Handled NullPointerException: " + e.getMessage());
         }
     }
 
     public static void main(String[] args) {
-<<<<<<< HEAD:Strings /Strings/NullPointerDemo.java
-=======
-      
->>>>>>> 3432d985465bb1a35e973a31f226f071c2136dc4:Strings/NullPointer.java
-        System.out.println("Demonstrating NullPointerException handling:");
-        handleNullPointer();
+        demonstrateNullPointerException();
     }
 }
