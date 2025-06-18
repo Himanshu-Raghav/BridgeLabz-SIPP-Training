9b50f1bf834fdaef7a019f1f4d1b00d34153e235
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-18 19:37:04 +0530

    [Himanshu] Added  a program to convert the complete text to uppercase and compare the results

--- Strings/ToUpperCaseCompare.java
+++ Strings/ToUpperCaseCompare.java
@@ -5,6 +5,7 @@ public class ToUpperCaseCompare {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
+            
             if (c >= 'a' && c <= 'z') {
                 sb.append((char)(c - 32));
             } else {
