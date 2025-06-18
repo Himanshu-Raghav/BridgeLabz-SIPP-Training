3432d985465bb1a35e973a31f226f071c2136dc4
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-18 11:54:51 +0530

    [Himanshu] Added  a program to demonstrate NULLPOINTER exception

--- Strings/NullPointer.java
+++ Strings/NullPointer.java
@@ -1,12 +1,12 @@
-public class Q2_NullPointerDemo {
-    // Method to generate NullPointerException
+public class NullPointerDemo {
+    
     public static void generateNullPointer() {
         String text = null;
-        // This will throw NullPointerException
+        
         System.out.println(text.length());
     }
 
-    // Method to handle NullPointerException
+   
     public static void handleNullPointer() {
         String text = null;
         try {
@@ -17,8 +17,7 @@ public class Q2_NullPointerDemo {
     }
 
     public static void main(String[] args) {
-        // Uncomment to see the exception
-        // generateNullPointer();
+      
         System.out.println("Demonstrating NullPointerException handling:");
         handleNullPointer();
     }
