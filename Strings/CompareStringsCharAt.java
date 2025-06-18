ab1fe4c2a61c3f7ebb2590bf162a43afa29d46b3
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-18 19:19:34 +0530

    [Himanshu] Added  a program to create a substring from a String using the charAt() method

--- Strings/CompareStringsCharAt.java
+++ Strings/CompareStringsCharAt.java
@@ -4,6 +4,7 @@ public class CompareStringsCharAt {
     public static boolean compareByCharAt(String s1, String s2) {
         if (s1.length() != s2.length()) return false;
         for (int i = 0; i < s1.length(); i++) {
+            
             if (s1.charAt(i) != s2.charAt(i)) return false;
         }
         return true;
