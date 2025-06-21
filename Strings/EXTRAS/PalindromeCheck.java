0c266ba29e92488997a866f7302941f48fdfaaf8
Himanshu-Raghav <132332625+Himanshu-Raghav@users.noreply.github.com>
2025-06-21 19:45:27 +0530

    [Himanshu] Added  a Java program to check if a given string is a palindrome

--- Strings/EXTRAS/PalindromeCheck.java
+++ Strings/EXTRAS/PalindromeCheck.java
@@ -7,6 +7,7 @@ public class PalindromeCheck {
         String str = sc.nextLine();
         String reversed = "";
 
+        
         for (int i = str.length() - 1; i >= 0; i--)
             reversed += str.charAt(i);
 
